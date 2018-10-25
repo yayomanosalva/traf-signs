30fc195e01b337d426c2c6b6a2e7eafed70a4a22
jair <yayomanosalva@gmail.com>
2018-10-25 13:37:18 -0500

    My First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'traf-signs';
+	title: string = 'traf-signs';
+	lat: number = 10.465082;
+	lng: number = -73.264308;
+	zoom: number = 13;
+	locationChosen = false;
+
+	onChoseLocation(event) {
+		console.log('eventos del click', event);
+		this.lat = event.coords.lat;
+		this.lng = event.coords.lng;
+		this.locationChosen = true;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [
+		BrowserModule,
+		AgmCoreModule.forRoot({
+			apiKey: 'AIzaSyCiyB5tqBmtcvoKTB-Bexl5oNuE5pG_h84'
+		})
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
